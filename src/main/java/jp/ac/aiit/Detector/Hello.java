74daa476dcb3a263df9f2e870e521f5a2eaceea2
sokky <mail>
2014-12-02 21:15:23 +0900

    initial commit
    gradle environment

---
+++ src/main/java/jp/ac/aiit/Detector/Hello.java
@@ -0,0 +1,15 @@
+package jp.ac.aiit.Detector;
+
+/**
+ * テスト用クラス。いずれ消す
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    public int plus(int arg) {
+        return arg + 5;
+    }
+}
---
+++ src/test/java/jp/ac/aiit/Detector/HelloTest.java
@@ -0,0 +1,15 @@
+package jp.ac.aiit.Detector;
+
+import junit.framework.TestCase;
+
+public class HelloTest extends TestCase {
+
+    /**
+     * Test用コード。いずれ消す
+     *
+     */
+    public void testPlus() throws Exception {
+
+        assert true;
+    }
+}
