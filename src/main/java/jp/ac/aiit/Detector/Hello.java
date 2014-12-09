ab46a1bff8a28019c7f49e7a92e41134d9108391
test <test@com>
2014-12-09 23:24:20 +0900

    add opencv library

--- src/main/java/jp/ac/aiit/Detector/Hello.java
+++ src/main/java/jp/ac/aiit/Detector/Hello.java
@@ -4,6 +4,12 @@ import jp.ac.aiit.Detector.util.Debug;
 
 import java.util.*;
 
+import org.bytedeco.javacpp.*;
+
+import static org.bytedeco.javacpp.opencv_core.*;
+import static org.bytedeco.javacpp.opencv_highgui.*;
+
+
 /**
  * テスト用クラス。いずれ消す
  */
@@ -21,6 +27,21 @@ public class Hello {
         Debug.debug("test;", m);
         Calendar cal = Calendar.getInstance();
         Debug.debug("test;" , cal);
+
+        opencv_objdetect.CascadeClassifier faceDetector
+                = new opencv_objdetect.CascadeClassifier(Hello.class.getResource("/lbpcascade_frontalface.xml").getPath().substring(1));
+        Mat image = imread(Hello.class.getResource("/lena.png").getPath().substring(1));
+
+        Rect faceDetections = new Rect();
+        faceDetector.detectMultiScale(image, faceDetections);
+
+        System.out.println(String.format("Detected %s faces", faceDetections));
+
+        rectangle(image, new Point(faceDetections.x(), faceDetections.y()), new Point(faceDetections.x() + faceDetections.width(), faceDetections.y() + faceDetections.height()), new Scalar(0, 255, 0, 0));
+
+        String filename = "build/faceDetection.png";
+        System.out.println(String.format("Writing %s", filename));
+        imwrite(filename, image);
     }
 
     public int plus(int arg) {
