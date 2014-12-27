ac76c624150fbad0813c196a58295cf68e37fc5d
sokky <phonemic.system@gmail.com>
2014-12-28 04:39:03 +0900

    add CvTest.java

--- src/test/java/jp/ac/aiit/Detector/CvTest.java
+++ src/test/java/jp/ac/aiit/Detector/CvTest.java
@@ -5,15 +5,18 @@ import jp.ac.aiit.Detector.util.Tool;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.junit.Test;
 
+import java.io.PrintStream;
+
 import static org.bytedeco.javacpp.opencv_highgui.imread;
 import static org.junit.Assert.*;
 
 public class CvTest {
 
     @Test
-    public void testCvLib() {
-        System.out.println("test:");
-        System.getProperties().list(System.out);
+    public void testCvLib() throws Exception{
+        PrintStream out = new PrintStream("log.log");
+
+        System.getProperties().list(out);
         Mat image = imread(Tool.getResourcePath(this.getClass(), "/lena.png"));
     }
 }
