da53ad3d56d73868f1845064259d58a2b2457ea4
sokky <phonemic.system@gmail.com>
2014-12-28 05:58:33 +0900

    fix travis.yml

--- src/test/java/jp/ac/aiit/Detector/CvTest.java
+++ src/test/java/jp/ac/aiit/Detector/CvTest.java
@@ -17,6 +17,8 @@ public class CvTest {
         System.setOut(out);
         System.getProperties().list(out);
 
+        System.load("/home/travis/build/pbl2014/Detector/lib/libopencv_core.so.2.4");
+
         Mat image = imread(Tool.getResourcePath(this.getClass(), "/lena.png"));
     }
 }
