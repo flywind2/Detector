3e160e220fe910a8fce0ad5843a0268c8b5ddaea
sokky <phonemic.system@gmail.com>
2014-12-28 06:29:34 +0900

    fix travis.yml

--- src/test/java/jp/ac/aiit/Detector/CvTest.java
+++ src/test/java/jp/ac/aiit/Detector/CvTest.java
@@ -4,27 +4,13 @@ import jp.ac.aiit.Detector.util.Tool;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.PrintStream;
-
 import static org.bytedeco.javacpp.opencv_highgui.imread;
 
 public class CvTest {
 
     @Test
     public void testCvLib() {
-        PrintStream out = null;
-        try {
-            out = new PrintStream("log.log");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        System.setOut(out);
-        System.getProperties().list(out);
-
-        //System.load("/home/travis/build/pbl2014/Detector/lib/libopencv_core.so.2.4");
-
+        // opencvライブラリ呼び出しテスト
         Mat image = imread(Tool.getResourcePath(this.getClass(), "/lena.png"));
     }
 }
