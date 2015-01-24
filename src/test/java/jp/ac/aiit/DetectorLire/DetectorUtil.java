57e03a53faaa6cd29db9a5c8d75a655c67eb128a
sokky <mail>
2015-01-24 16:06:36 +0900

    fix Lire

--- src/test/java/jp/ac/aiit/DetectorLire/DetectorUtil.java
+++ src/test/java/jp/ac/aiit/DetectorLire/DetectorUtil.java
@@ -73,7 +73,7 @@ public class DetectorUtil {
             try {
                 ArrayList<String> images = getAllImages(new File(targetDir), true);
                 IndexWriter iw = LuceneUtils.createIndexWriter(path, true);
-                DocumentBuilder builder = DocumentBuilderFactory.getFullDocumentBuilder();
+                DocumentBuilder builder = DocumentBuilderFactory.getCEDDDocumentBuilder();
                 for (String identifier : images) {
                     Document doc = builder.createDocument(new FileInputStream(identifier), identifier);
                     iw.addDocument(doc);
